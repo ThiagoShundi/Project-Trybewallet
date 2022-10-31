cdb574d4319e81627a6e690f3b62499f9e381a4c
ThiagoShundi <shundi_hayashi@hotmail.com>
2022-10-31 19:14:03 -0300

    Correção do Requisito 2.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      totalSpent: 0,
+      exchange: 'BRL',
+    };
+  }
+
   render() {
+    const { email } = this.props;
+    const { totalSpent, exchange } = this.state;
+
     return (
-      <div>Header</div>
+      <>
+        <div>TrybeWallet</div>
+        <div data-testid="email-field">{ email }</div>
+        <div data-testid="total-field">
+          Total de despesas:
+          {' '}
+          { totalSpent }
+        </div>
+        <div data-testid="header-currency-field">{ exchange }</div>
+      </>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,42 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      totalSpent: 0,
-      exchange: 'BRL',
-    };
-  }
-
   render() {
-    const { email } = this.props;
-    const { totalSpent, exchange } = this.state;
-
     return (
       <>
         <div>TrybeWallet</div>
-        <div data-testid="email-field">{ email }</div>
-        <div data-testid="total-field">
-          Total de despesas:
-          {' '}
-          { totalSpent }
-        </div>
-        <div data-testid="header-currency-field">{ exchange }</div>
+        <Header />
       </>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  email: state.user.email,
-});
-
-Wallet.propTypes = {
-  email: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps)(Wallet);
+export default Wallet;
