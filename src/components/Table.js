f49159c1ca22eed1a470219c4ea5033013ace42c
ThiagoShundi <shundi_hayashi@hotmail.com>
2022-11-07 17:23:19 -0300

    Requisito 7 Finalizado.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
+  handleClick = () => {
+    // const { expenses } = this.props;
+    // const select = document.querySelectorAll('.selected');
+    // select.remove();
+  };
+
   render() {
+    const { expenses } = this.props;
+
     return (
       <>
         <div>Table</div>
         <table>
-          <tbody>
+          <thead>
             <tr>
               <th>Descrição</th>
               <th>Tag</th>
@@ -18,6 +28,40 @@ class Table extends Component {
               <th>Moeda de conversão</th>
               <th>Editar/Excluir</th>
             </tr>
+          </thead>
+          <tbody>
+            {
+              expenses.map((exp, index) => (
+                <tr key={ index }>
+                  <td>{ exp.description }</td>
+                  <td>{ exp.tag }</td>
+                  <td>{ exp.method }</td>
+                  <td>{ Number(exp.value).toFixed(2) }</td>
+                  <td>
+                    {
+                      exp.exchangeRates[exp.currency].name
+                    }
+                  </td>
+                  <td>{ (Number(exp.exchangeRates[exp.currency].ask)).toFixed(2) }</td>
+                  <td>
+                    {
+                      (Number(exp.exchangeRates[exp.currency].ask)
+                        * Number(exp.value)).toFixed(2)
+                    }
+                  </td>
+                  <td>Real</td>
+                  <td>
+                    <button
+                      data-testid="delete-btn"
+                      type="button"
+                      onClick={ this.handleClick }
+                    >
+                      Excluir
+                    </button>
+                  </td>
+                </tr>
+              ))
+            }
           </tbody>
         </table>
       </>
@@ -25,4 +69,12 @@ class Table extends Component {
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({ value: PropTypes.string })).isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
